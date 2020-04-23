2df2f5a7cde6665d9753ed19234e4ef41009ad2c
FlyMeToTheMars <flyhobo@live.com>
2020-04-23 08:08:06 +0800

    base v0.1

---
+++ hugh_01_base/fh_00_base.py
@@ -0,0 +1,21 @@
+# code comments
+print("python hello world")
+
+"""
+this is comments too
+"""
+
+"""
+Google Python Coding Style Guide:
+    https://zh-google-styleguide.readthedocs.io/en/latest/google-python-styleguide/python_style_rules/
+"""
+
+# 算术运算符 + - * / // % **
+
+# 字符串和算术运算符
+print('-------' * 10)
+
+# 变量
+acount = "flyhugh"
+
+# 进度 lesson 057
