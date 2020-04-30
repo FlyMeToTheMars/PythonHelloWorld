7a199e1d4849a27af26c179e84ba3043b41ac08c
FlyMeToTheMars <flyhobo@live.com>
2020-04-30 20:24:26 +0800

    base v0.1

--- hugh_01_base/fh_00_base.py
+++ hugh_01_base/fh_00_base.py
@@ -18,4 +18,17 @@ print('-------' * 10)
 # 变量
 acount = "flyhugh"
 
+# 变量的类型
+# 定义小明 不需要制定变量类型 系统会自动推导
+name = "小明"
+age = 18
+gender = True
+
+# DEBUG 每次重新开一个 都会重新开启一个内存区域
+
+# 类型转换
+int("123")
+print(type(int("123")))
+print(type(float("12.3")))
+
 # 进度 lesson 057
